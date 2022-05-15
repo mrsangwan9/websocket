b2730efc33370876aa158db0ebfe077637186436
aryan <aryasangwan00@gmail.com>
2022-05-15 08:35:08 +0530

    comments adde for better view

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,21 +5,21 @@ from time import sleep # import sleep for stop process for one second or as slee
 import asyncio # use sleep time in asyncoConsumer we have to use asyncio.sleep(time in second )
 
 class MySyncConsumer(SyncConsumer):
-    def websocket_connect(self,event):
+    def websocket_connect(self,event):# this  occur when websocket.connect event happen after open websocket by js
         print('websocket connected...',event)
         self.send({
-            'type':'websocket.accept',
+            'type':'websocket.accept',# websocket connection accept by the server now we can share data both side.
         })
 
-    def websocket_receive(self,event):
-        print("message received from client", ['text'])
-        print( ['text'])
-        for i in range(15):
+    def websocket_receive(self,event): # that function run after receive data from the client like message somethin
+        print("message received from client", ['text']) # print the message.
+        print( event['text'])# print message receive from
+        for i in range(7):
             self.send({
-                "type":"websocket.send",
-                'text':str(i)
+                "type":"websocket.send",# send data to client
+                'text':str(i)# send i value to client // data must be a string..
              })
-            sleep(1)
+            sleep(1) # timer for repeate message after interval of time
 
     def websocket_disconnect(self,event):
         print("connection is disconnect...", event)
@@ -38,9 +38,9 @@ class MyAsyncConsumer(AsyncConsumer):
          for i in range(15):
             await self.send({
                 "type":"websocket.send",
-                'text':str(i)
+                'text':str(i) 
              })
-        asyncio.sleep(1)
+         asyncio.sleep(1)
 
         async def websocket_disconnect(self,event):
           print("connection is disconnect")
